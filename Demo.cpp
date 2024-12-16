90bcb54ce2b7ab07b4655ec559f3d8f172167ad4
yda <yangda@seer-group.com>
2024-12-16 12:14:57 +0100

    Add .editorconfig

--- Demo.cpp
+++ Demo.cpp
@@ -3,10 +3,10 @@
 
 
 int main() {
-	Person person;
-	person.set_name("John Doe");
-	person.set_gender("M");
-	person.set_email("j.doe@some.com");
-	std::cout << person.ShortDebugString() << std::endl;
-	return 0;
+    Person person;
+    person.set_name("John Doe");
+    person.set_gender("M");
+    person.set_email("j.doe@some.com");
+    std::cout << person.ShortDebugString() << std::endl;
+    return 0;
 }
